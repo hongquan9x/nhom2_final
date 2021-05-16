1b2cdaf8c287c1ce71431ffea3189409731b5a9b
hongquan9x <redarmy92@gmail.com>
2021-05-16 23:10:03 +0700

    +security

---
+++ src/main/java/com/nhom2/cinemamanagement/control/WebController.java
@@ -0,0 +1,14 @@
+package com.nhom2.cinemamanagement.control;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    public String homePage(){
+        return "index";
+    }
+}
